f3840b56a9b4fe0b43c8e469cbf7d6315db8444d
Maidulweb <maidulimv@gmail.com>
2022-06-06 15:05:18 +0600

    Data added

---
+++ src/shop/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <Header />
+            <div className=''></div>
+        </div>
+    );
+};
+
+export default Shop;
